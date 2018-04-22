78f14ff9da3e1f70a79577d4f19c8d951aa25432
Howard Yeh <howard@metacircus.com>
2018-04-22 09:58:22 +0800

    qtumjs scritps qtumjs scripts

---
+++ index.js
@@ -0,0 +1,87 @@
+const { Qtum } = require("qtumjs")
+const BN = require("bn.js")
+
+const repoData = require("./solar.development.json")
+
+const qtum = new Qtum("http://qtum:test@localhost:3889", repoData)
+
+const mytoken = qtum.contract("VevueToken.sol")
+
+async function showInfo() {
+  const decimals = await mytoken.returnNumber("decimals")
+  const totalSupply = await mytoken.return("totalSupply")
+  const saleAmount = await mytoken.return("saleAmount")
+  const tokensSold = await mytoken.return("tokensSold")
+
+  const tokenTotalSupply = (await mytoken.call("tokenTotalSupply")).outputs[0]
+
+  function tokenAmount(bnumber) {
+    const nstr = bnumber.toString()
+    const amountUnit = nstr.substring(0, nstr.length - decimals)
+    const amountDecimals = nstr.substring(nstr.length - decimals)
+
+    return `${amountUnit === "" ? 0 : amountUnit}.${amountDecimals}`
+  }
+
+  console.log("supply cap:", tokenAmount(tokenTotalSupply))
+  console.log("sales cap:", tokenAmount(saleAmount))
+  console.log("current token supply:", tokenAmount(totalSupply))
+  console.log("tokens sold:", tokenAmount(tokensSold))
+}
+
+/**
+ *
+ * @param {string} beneficiary address to send purchase tokens
+ * @param {number} amount amount of qtum used to purchase tokens
+ */
+async function buyTokens(beneficiary, amount) {
+  const tx = await mytoken.send("buyTokens", [beneficiary], {
+    amount,
+  })
+
+  const res = await tx.confirm(1)
+  console.log(res)
+}
+
+async function mintTokens(amount) {
+  const tx = await mytoken.send("mintReservedTokens", [amount])
+  const res = await tx.confirm(1)
+  console.log(res)
+}
+
+async function main() {
+  const argv = process.argv.slice(2)
+
+  const cmd = argv[0]
+
+  if (process.env.DEBUG) {
+    console.log("argv", argv)
+    console.log("cmd", cmd)
+  }
+
+  switch (cmd) {
+    case "info":
+      await showInfo()
+      break
+    case "buy":
+      const buyAmount = parseFloat(argv[2])
+      if (!buyAmount) {
+        throw new Error(`Invalid amount: ${argv[2]}`)
+      }
+      await buyTokens(argv[1], buyAmount)
+      break
+    case "mint":
+      const mintAmount = parseFloat(argv[1])
+      if (!mintAmount) {
+        throw new Error(`Invalid amount: ${argv[1]}`)
+      }
+      await mintTokens(mintAmount)
+      break
+    default:
+      console.log("unrecognized command", cmd)
+  }
+}
+
+main().catch((err) => {
+  console.log("err", err)
+})
